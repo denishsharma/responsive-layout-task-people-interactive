5b5b10f51f13119acb7657cfd4132e35c9b15bae
Denish Sharma <denishcommon@gmail.com>
2025-07-24 11:55:36 +0530

    chore: setup base vanilla project

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu({
+  formatters: true,
+  stylistic: {
+    indent: 2,
+    semi: false,
+    quotes: 'single',
+    overrides: {
+      'style/comma-dangle': ['error', 'always-multiline'],
+      'style/array-bracket-newline': ['error', { multiline: true, minItems: 3 }],
+      'style/function-call-argument-newline': ['error', 'consistent'],
+      'style/brace-style': [
+        'error',
+        '1tbs',
+        { allowSingleLine: true },
+      ],
+      'style/max-statements-per-line': ['error', { max: 2 }],
+      'style/wrap-regex': 'error',
+      'style/member-delimiter-style': 'error',
+    },
+  },
+  lessOpinionated: true,
+})
